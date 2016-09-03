297ab215d4f3c7c876918935363f2a9f7bcb4dce
Dusan Tuzinsky <dusan.tuzinsky@gmail.com>
2016-09-03 14:54:56 +0200

    Fluid band container

--- javascript/tamplates.js
+++ javascript/tamplates.js
@@ -4,7 +4,9 @@ var htmlTemplates = {
     bandsWrapper: '<div class="ui tab" data-tab="bands-{band.lang}" ></div>',
 
     band:  '<div id="band-{band.id}" class="ui title band-content-header">\
-                <h3 class="ui header" >{band.name}<span class="ui circular label">{band.badge}</span></h3>\
+                <div class="ui center aligned container">\
+                    <h3 class="ui header" >{band.name}<span class="ui circular label">{band.badge}</span></h3>\
+                </div>\
             </div>\
             <div class="ui content band-content-wrapper" >\
                 <div class="accordion">\
@@ -13,7 +15,9 @@ var htmlTemplates = {
             </div>',
 
     song:  '<div id="song-{song.id}" class="ui title song-content-header">\
-                <h4 class="ui header">{song.title}</h4>\
+                <div class="ui center aligned container">\
+                    <h4 class="ui header">{song.title}</h4>\
+                </div>\
             </div>\
             <div class="ui content left aligned container song-content-wrapper">\
                 {song.content}\
